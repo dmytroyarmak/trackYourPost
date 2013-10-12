7161471003b6580e7245828ed07372da38747692
Dmytro Yarmak <dyarmak@heliostech.hk>
2013-10-12 19:43:17 +0300

    Fix secure problem with new Firefox

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -1,4 +1,4 @@
 define({
     dropboxAppKey: '04b5gvby9ws1a8o',
-    ukrPostApiUrl: 'http://mighty-fjord-4086.herokuapp.com/tracks'
+    ukrPostApiUrl: 'https://mighty-fjord-4086.herokuapp.com/tracks'
 });
